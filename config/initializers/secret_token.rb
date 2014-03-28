ae583ce0e4bb09c31b603548fd868dd19e77cf9a
CarlaAstudillo <carla.a.astudillo@gmail.com>
2014-03-28 14:40:43 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sandy::Application.config.secret_key_base = '585e51b44fab04b848ea1ef73a16764ee599d493cb4de9b141e7c60d334cda3b49b64111f3fe6958d514e28a20e9e16d47ca76e6ce4e4b8bffd104d348e73a5b'
