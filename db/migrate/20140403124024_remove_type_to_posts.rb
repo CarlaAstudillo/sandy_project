8f356b2c6e2279655c396ec2642788812f4cd3ad
CarlaAstudillo <carla.a.astudillo@gmail.com>
2014-06-26 14:42:09 -0400

    Removed type and kind removal migrations

--- db/migrate/20140403124024_remove_type_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeToPosts < ActiveRecord::Migration
-  def change
-    remove_column :posts, :type, :string
-  end
-end
--- db/migrate/20140403125018_remove_kind_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveKindToPosts < ActiveRecord::Migration
-  def change
-    remove_column :posts, :kind, :string
-  end
-end
